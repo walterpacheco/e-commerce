43c206a7907177280224d14a81e1cf66c41d3e58
Walter Pacheco <walter.e.pacheco.chandia@gmail.com>
2019-07-19 02:43:57 -0400

    Se agrega cancancan y se limita los permisos para borrar y agregar productos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.html { redirect_to root_url, alert: exception.message }
+    end
+  end
+  
   protected
-
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
   end
